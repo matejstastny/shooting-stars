b442420b29cd6b6571e85c2dff5e084717496d6a
Matěj Šťastný <matysta@outlook.com>
2025-03-08 13:49:45 -0800

    Switched from johnrengelman shadow to gradleup shadow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ val projectName = "shooting-stars"
 plugins {
     id("application")
     id("java")
-    id("com.github.johnrengelman.shadow") version "8.1.1"
+    id("com.gradleup.shadow") version "9.0.0-beta10"
 }
 
 repositories {
@@ -14,9 +14,9 @@ repositories {
 dependencies {
     //---- JSON file manipulation ----
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
-    implementation ("com.fasterxml.jackson.core:jackson-databind:2.12.3")
-    implementation ("com.fasterxml.jackson.core:jackson-core:2.12.3")
-    implementation ("com.fasterxml.jackson.core:jackson-annotations:2.12.3")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.3")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.12.3")
+    implementation("com.fasterxml.jackson.core:jackson-annotations:2.12.3")
     //---- XLSX spreadsheet manipulation ----
     implementation("org.apache.poi:poi-ooxml:5.2.3")
     implementation("org.apache.logging.log4j:log4j-core:2.20.0")
@@ -40,19 +40,18 @@ tasks.named<Test>("test") {
     useJUnitPlatform()
 }
 
-tasks.withType<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar> {
-    enabled = true
+tasks.shadowJar {
     archiveBaseName.set(projectName)
-    archiveVersion.set("")
     archiveClassifier.set("")
+    archiveVersion.set("")
     doLast {
         println("Shadow JAR has been created at: ${archiveFile.get().asFile.absolutePath}")
     }
 }
 
 tasks.named<JavaExec>("run") {
-    dependsOn(tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar"))
-    classpath = sourceSets["main"].runtimeClasspath + files(tasks.named<com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar>("shadowJar").get().archiveFile)
+    dependsOn(tasks.shadowJar)
+    classpath = sourceSets["main"].runtimeClasspath + files(tasks.shadowJar.get().archiveFile)
     mainClass.set(application.mainClass)
 }
 
